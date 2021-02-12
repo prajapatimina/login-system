a02fd867dc484e7ee1b1107c1429fe5c8d6a9023
prajapatimina <prajapatimina37@gmail.com>
2021-02-12 22:21:36 +0545

    initial commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req,res,next)=>{
     error.status = 404;
     next(error);
 })
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
 })
